b9fe437c19a891ba603009b334d4e34e0de0d2b8
Mehulpatangya <mehuljainind@gmail.com>
2021-08-19 21:32:47 +0530

    git pull issue

--- src/screen/MainScreen.js
+++ src/screen/MainScreen.js
@@ -2,7 +2,7 @@ import Landing from "../components/LandingPage";
 import Mentors from "../components/Mentors";
 import Process from "../components/OurProcess";
 import Mentorship from '../components/Mentorship';
-import Services from "../components/OurServices"
+import Services from "../components/ourServices"
 import SuccessStories from "../components/SuccessStories";
 import "../style/MainScreen.css";
 
